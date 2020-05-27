ac4b42f042e216709f6691f428595c32a3dbe376
doreilly <doreilly@deloitte.co.nz>
2020-05-27 13:19:05 +1000

    first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './App.css';
+import lottie from 'lottie-web';
+import animationData from './lottie-files/birds.json';
+
+let animObj = null;
+
+class App extends React.Component {
+  componentDidMount() {
+    console.log('componentDidMount');
+    
+    //call the loadAnimation to start the animation
+    animObj = lottie.loadAnimation({
+      container: this.animBox, // the dom element that will contain the animation
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      animationData: animationData // the path to the animation json
+    });
+  }
+  handleStop = () => {
+    animObj.stop();
+  }
+  handlePlay() {
+    animObj.play();
+  }
+  render() {
+    return (
+      <div className="App">
+        <h2>This is my Lottie Web animation</h2>
+        {/* This is you wrapper where animation will load */}
+        <div style={{width: '50%', margin: '0 auto'}} ref={ ref => this.animBox = ref}></div>
+        <button onClick={this.handleStop}>Stop</button>
+        <button onClick={this.handlePlay}>Play</button>
+      </div>
+    );
+  }
+}
+
+export default App;
